97063933fb3c5bd0e92348ed5945725a3ba07d5f
Pacific89 <philipp.faulhammer@medunigraz.at>
2021-04-23 10:30:54 +0200

    add logos

--- getlogos.py
+++ getlogos.py
@@ -39,16 +39,78 @@
 
 from selenium import webdriver
 import urllib
-driver = webdriver.Firefox()
-driver.get('http://catalogue.rd-connect.eu/web/guest/catalogue')
-
-name = driver.find_element_by_xpath('//div[@id="img_id"]/img')
-print(name)
-
-img = driver.find_element_by_xpath('img_id=')
-src = img.get_attribute('src')
-urllib.urlretrieve(src, "captcha.png")
+import requests
+import re
+import json
+import os
+import time
 
 
-# with open('filename.png', 'wb') as file:
-#     file.write(driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/table[1]/tbody/tr/td[1]/a/div').screenshot_as_png)
+def get_links():
+    # driver = webdriver.Firefox(executable_path="/home/user/Downloads/geckodriver-v0.29.1-linux32/geckodriver")
+    # driver.get('http://catalogue.rd-connect.eu/web/guest/catalogue')
+
+    # example logo link
+    # http://catalogue.rd-connect.eu/image/layout_set_logo?img_id=203988
+    #
+
+    # name = driver.find_element_by_xpath(("//*[contains(text(),'img_id')]"))
+    url = 'http://catalogue.rd-connect.eu/web/guest/catalogue'
+    # find occurance of img_id links:
+    text = requests.get(url).text
+    print(text)
+    organisation_mask = re.compile(r"{Name:\".*?}")
+    found = organisation_mask.findall(text)
+
+    with open('organisations.json', 'w') as f:
+        json.dump(found, f)
+
+
+    # img = driver.find_element_by_xpath('img_id=')
+    # src = img.get_attribute('src')
+    # urllib.urlretrieve(src, "captcha.png")
+
+
+    # with open('filename.png', 'wb') as file:
+    #     file.write(driver.find_element_by_xpath('/html/body/div[1]/div[5]/div[2]/table[1]/tbody/tr/td[1]/a/div').screenshot_as_png)
+
+def get_files():
+    print("read")
+    with open("organisations.json") as f:
+        organisations = json.load(f)
+        
+    link_mask = re.compile(r"OrganizationImageLink:\".*?\"")
+    name_mask = re.compile(r"Name:\".*?\"")
+
+    os.chdir("logos")
+    k=1
+    for org in organisations:
+        img_url = link_mask.findall(org)[0].split("\"")[1]
+        name = name_mask.findall(org)[0]
+        # print("{0} : {1}".format(name_mask.findall(org)[0], img_url))
+        link = "http://catalogue.rd-connect.eu" + img_url
+        file_name = link.split("=")
+        if len(file_name) > 1:
+            file_name = file_name[-1]
+        else:
+            file_name = file_name[0].split("/")[-1][:-4]
+
+        print("{0} / {1} -- {2}".format(k, len(organisations), name))
+        print(file_name)
+        with urllib.request.urlopen(link) as response:
+            info = response.info()
+            file_type = info.get_content_type().split("/")[-1]
+            file_ext = "." + file_type
+        
+        if os.path.exists(file_name+file_ext):
+            print("Skip: ", file_name+file_ext)
+            continue
+        else:
+            print("Get: ", file_name+file_ext)
+            urllib.request.urlretrieve(link, file_name)
+
+            time.sleep(1.0)
+        k+=1
+if __name__ == "__main__" :
+    # get_links()
+    get_files()
